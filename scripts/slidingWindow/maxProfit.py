ac74267027883e8f5831db9d3783cda55ede748b
varunkhanna1993 <varunkhanna93@hotmail.com>
2022-06-10 02:44:49 +1200

    maxProfit, sliding window example

---
+++ scripts/slidingWindow/maxProfit.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices):
+        l, r = 0,1
+        maxP = 0
+        while r < len(prices):
+            #is it profitable?
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l+=r
+            r+=1
+
+        return maxP
+        
+
+prices = [7,1,5,3,6,4]
+# prices = [7,6,4,3,1]
+print(Solution().maxProfit(prices))
